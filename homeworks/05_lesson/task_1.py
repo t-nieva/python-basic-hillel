a7c10217794201a1a20f78638d26ba3e8bfd0add
Tetiana Niehrieieva <t.niehrieieva@gmail.com>
2025-06-13 10:44:44 +0300

    Reworked task_1.py, lesson 5

--- homeworks/05_lesson/task_1.py
+++ homeworks/05_lesson/task_1.py
@@ -1,64 +1,23 @@
-string_punctuation = """!"#$%&'()*+,-./:;<=>?@[\]^`{|}~"""
-kwlist = [
-    "False",
-    "None",
-    "True",
-    "and",
-    "as",
-    "assert",
-    "async",
-    "await",
-    "break",
-    "class",
-    "continue",
-    "def",
-    "del",
-    "elif",
-    "else",
-    "except",
-    "finally",
-    "for",
-    "from",
-    "global",
-    "if",
-    "import",
-    "in",
-    "is",
-    "lambda",
-    "nonlocal",
-    "not",
-    "or",
-    "pass",
-    "raise",
-    "return",
-    "try",
-    "while",
-    "with",
-    "yield",
-]
-soft_kwlist = ["case", "match", "type"]
+import keyword
+import string
 
 
 def validate_variable(variable_name: str) -> bool:
-    underscore_count = 0
-
     if variable_name[0].isdigit():
         return False
 
-    if variable_name in kwlist or variable_name in soft_kwlist:
+    if keyword.iskeyword(variable_name):
+        return False
+
+    if 1 < len(variable_name) == variable_name.count("_"):
         return False
 
     for letter in variable_name:
         if letter.isupper():
             return False
-        if letter == " ":
+        if letter in string.whitespace:
             return False
-        if letter in string_punctuation:
+        if letter in string.punctuation and letter != "_":
             return False
-        if letter == "_":
-            underscore_count += 1
-
-    if underscore_count > 1:
-        return False
 
     return True
