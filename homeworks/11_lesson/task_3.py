5a65d7447c8ac760d2034a8fcfd0c8df00d2fca3
Tetiana Niehrieieva <t.niehrieieva@gmail.com>
2025-07-04 09:53:06 +0300

    Add hw 11, task 3. Additional task 21.py

---
+++ homeworks/11_lesson/task_3.py
@@ -0,0 +1,13 @@
+# def is_even(number):
+#     return (number & 1) == 0
+
+
+def is_even(number):
+    binary_str = bin(number)
+    return binary_str[-1] == "0"
+
+
+assert is_even(2494563894038**2) == True, "Test1"
+assert is_even(1056897**2) == False, "Test2"
+assert is_even(24945638940387**3) == False, "Test3"
+print("Ok")
