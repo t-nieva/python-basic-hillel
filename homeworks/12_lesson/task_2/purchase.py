2bf4e5080a9a4822f7b9d688138eea8a19012750
Tetiana Niehrieieva <t.niehrieieva@gmail.com>
2025-07-07 16:48:38 +0300

    Rework task 12.2, lesson 12

--- homeworks/12_lesson/task_2/purchase.py
+++ homeworks/12_lesson/task_2/purchase.py
@@ -4,15 +4,15 @@ class Purchase:
         self.user = user
         self.total = 0
 
-    def add_item(self, item, cnt):
-        self.products[item] = cnt
-
     def __str__(self):
         customer = f"User: {self.user}\n"
         items = ""
         for item in self.products:
             items += f"{item.name}: {self.products.get(item)} pcs.\n"
         return f"{customer}Items:\n{items}"
+
+    def add_item(self, item, cnt):
+        self.products[item] = cnt
 
     def get_total(self):
         total_price = 0
