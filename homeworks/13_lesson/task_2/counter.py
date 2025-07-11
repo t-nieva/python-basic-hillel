717b8bda22003b8c0720c1ea972924c6c038dad7
Tetiana Niehrieieva <t.niehrieieva@gmail.com>
2025-07-11 09:22:38 +0300

    Rework task 13.2, lesson 13

--- homeworks/13_lesson/task_2/counter.py
+++ homeworks/13_lesson/task_2/counter.py
@@ -1,17 +1,29 @@
 class Counter:
 
     def __init__(self, current=1, min_value=0, max_value=10):
+        if not (min_value <= current <= max_value):
+            raise ValueError("Initial current must be within min and max bounds.")
         self.current = current
         self.min_value = min_value
         self.max_value = max_value
 
     def set_current(self, start):
+        if not (self.min_value <= start <= self.max_value):
+            raise ValueError("Current value must be within min and max bounds.")
         self.current = start
 
     def set_max(self, max_max):
+        if max_max < self.min_value:
+            raise ValueError("max_value cannot be less than min_value.")
+        if self.current > max_max:
+            raise ValueError("Current value is out of the new max bound.")
         self.max_value = max_max
 
     def set_min(self, min_min):
+        if min_min > self.max_value:
+            raise ValueError("min_value cannot be greater than max_value.")
+        if self.current < min_min:
+            raise ValueError("Current value is out of the new min bound.")
         self.min_value = min_min
 
     def step_up(self):
